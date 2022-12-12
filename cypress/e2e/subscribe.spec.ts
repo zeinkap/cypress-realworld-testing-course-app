1c8733087cb78e51eb41f14f501d967c827ffb36
Zein <zeinkap@gmail.com>
2022-12-12 16:41:24 -0500

    update tests for subscribe

--- cypress/e2e/subscribe.spec.ts
+++ cypress/e2e/subscribe.spec.ts
@@ -8,15 +8,22 @@ describe("Newsletter Subscribe Form", () => {
   const SUBSCRIBED_EMAIL = "john@example.com"
 
   context("happy paths", () => {
-    it("the email input field contains the correct placeholder text", () => {
+    it("email field contains the correct placeholder text", () => {
       cy.getByData("email-input")
         .invoke("attr", "placeholder")
         .should("eq", "Subscribe for Updates")
     })
 
     it("allows users to subscribe to the email list", () => {
-      cy.getByData("email-input").type(NEW_EMAIL)
+      cy.intercept("POST", "/api/subscribe", {
+        statusCode: 200,
+        body: {
+          message: `Success: ${NEW_EMAIL} has been successfully subscribed`,
+        },
+      }).as("emailSubscribe")
+      cy.getByData("email-input").type(NEW_EMAIL).blur()
       cy.getByData("submit-button").click()
+      cy.wait("@emailSubscribe").its("response.statusCode").should("eq", 200)
       cy.getByData("success-message").should("exist").contains(NEW_EMAIL)
     })
   })
@@ -29,8 +36,16 @@ describe("Newsletter Subscribe Form", () => {
     })
 
     it("does NOT allow already subscribed email addresses", () => {
+      cy.intercept("POST", "/api/subscribe", {
+        statusCode: 403,
+        body: {
+          message: `Error: ${SUBSCRIBED_EMAIL} already exists. Please use a different email address.`,
+        },
+      }).as("emailSubscribe")
+
       cy.getByData("email-input").type(SUBSCRIBED_EMAIL)
       cy.getByData("submit-button").click()
+      cy.wait("@emailSubscribe").its("response.statusCode").should("eq", 403)
       cy.getByData("server-error-message")
         .should("exist")
         .contains(SUBSCRIBED_EMAIL)
@@ -38,6 +53,7 @@ describe("Newsletter Subscribe Form", () => {
 
     it("does NOT allow an empty email address", () => {
       cy.getByData("submit-button").click()
+      cy.getByData("success-message").should("not.exist")
       cy.getByData("error-message")
         .should("exist")
         .contains("Email is required")
